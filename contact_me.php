bad5a4cebaa0bd927051cabb686b882ea568280d
tjf081 <rickyfahey@hotmail.com>
2016-12-05 19:24:11 -0500

    file cleanup

--- contact_me.php
+++ contact_me.php
@@ -2,6 +2,31 @@
 require 'vendor/autoload.php';
 
 
+
+// If you are not using Composer (recommended)
+// require("path/to/sendgrid-php/sendgrid-php.php");
+
+$from = new SendGrid\Email(null, "tjf20@hotmail.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "rickyfahey@hotmail.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
+
+
+/*
+
+
+
+
+
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -38,7 +63,7 @@ $email->addTo("rickyfahey@hotmail.com")
 
 $sendgrid->send($email);
 
-
+*/
 
 
 
