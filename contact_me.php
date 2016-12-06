51c23badb559a0a9811c6dc6cb6b2b9e71528f3f
tjf081 <rickyfahey@hotmail.com>
2016-12-05 20:21:15 -0500

    file cleanup

--- contact_me.php
+++ contact_me.php
@@ -3,11 +3,6 @@ require 'vendor/autoload.php';
 
 
 
-return false;
-
-
-
-
 
 $request_body = json_decode('{
   "personalizations": [
@@ -31,7 +26,7 @@ $request_body = json_decode('{
   ]
 }');
 
-$apiKey = getenv('SENDGRID_API_KEY');
+$apiKey = "RSz21wAJTG29kwgxYdM-kA";  //getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($request_body);
@@ -39,52 +34,8 @@ echo $response->statusCode();
 echo $response->body();
 echo $response->headers();
 
-/*
 
 
-
-
-
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'rickyfahey@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-
-//mail($to,$email_subject,$email_body,$headers);
-
-$sendgrid = new SendGrid("RSz21wAJTG29kwgxYdM-kA");
-$email    = new SendGrid\Email();
-
-$email->addTo("rickyfahey@hotmail.com")
-      ->setFrom(strip_tags(htmlspecialchars($_POST['email'])))
-      ->setSubject($email_subject)
-      ->setHtml($email_body);
-
-$sendgrid->send($email);
-
-*/
-
-
-
-
-return true;         
+return false;
+//return true;         
 ?>
