77585050ef60d0f8cb19aa87b30efcdf9ecfe800
tjf081 <rickyfahey@hotmail.com>
2016-12-10 16:19:40 -0500

    updating the sendgrip path

--- contact_me.php
+++ contact_me.php
@@ -2,19 +2,32 @@
 require("sendgrid-php/sendgrid-php.php");
 
 
-$from = new SendGrid\Email(null, "tjf20@hotmail.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "rickyfahey@hotmail.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+try {
+    
+        $from = new SendGrid\Email(null, "tjf20@hotmail.com");
+        $subject = "Hello World from the SendGrid PHP Library!";
+        $to = new SendGrid\Email(null, "rickyfahey@hotmail.com");
+        $content = new SendGrid\Content("text/plain", "Hello, Email!");
+        $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
+        $apiKey = getenv('SENDGRID_API_KEY');
+        $sg = new \SendGrid($apiKey);
 
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+        $response = $sg->client->mail()->send()->post($mail);
+        echo $response->statusCode();
+        echo $response->headers();
+        echo $response->body();
+
+
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+        //file_put_contents("php://stderr", $e->getMessage());
+    } finally {
+        echo "First finally.\n";
+    }
+
+
+
 
 
 return true;         
