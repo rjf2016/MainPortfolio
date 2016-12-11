4fc3d43fe8643b7d26c24637bdb016a3653a38c7
tjf081 <rickyfahey@hotmail.com>
2016-12-11 08:33:13 -0500

    updating the sendgrip path

--- contact_me.php
+++ contact_me.php
@@ -3,7 +3,8 @@ require("sendgrid-php/sendgrid-php.php");
 
 
 try {
-    
+    /*
+        $host = 'https://rickfahey.herokuapp.com';
         $from = new SendGrid\Email(null, "tjf20@hotmail.com");
         $subject = "Hello World from the SendGrid PHP Library!";
         $to = new SendGrid\Email(null, "rickyfahey@hotmail.com");
@@ -17,6 +18,23 @@ try {
         echo $response->statusCode();
         echo $response->headers();
         echo $response->body();
+*/
+    $from = new Email(null, "tjf20@hotmail.com");
+    $subject = "Hello World from the SendGrid PHP Library";
+    $to = new Email(null, "rickyfahey@hotmail.com");
+    $content = new Content("text/plain", "some text here");
+    $mail = new Mail($from, $subject, $to, $content);
+    $to = new Email(null, "rickyfahey@hotmail.com");
+    $mail->personalization[0]->addTo($to);
+
+    $apiKey = getenv('SENDGRID_API_KEY');
+    $sg = new \SendGrid($apiKey);
+
+    $request_body = $mail;
+    $response = $sg->client->mail()->send()->post($request_body);
+    echo $response->statusCode();
+    echo $response->body();
+    echo $response->headers();
 
 
     } catch (Exception $e) {
@@ -24,6 +42,7 @@ try {
         file_put_contents("php://stderr", $e->getMessage()+"\n");
     } finally {
         file_put_contents("php://stderr", "done\n");
+        file_put_contents("php://stderr", $response->statusCode()+"\n");
     }
 
 
